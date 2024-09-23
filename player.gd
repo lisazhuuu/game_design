00fbe5aca04cf76a0e87cf65d85a613d298ac7fc
Mingfang Zhu <2316311284@qq.com>
2024-09-22 22:20:17 -0600

    initial commit

---
+++ player.gd
@@ -0,0 +1,67 @@
+extends Area2D
+signal hit
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		$AnimatedSprite2D.animation = "walk"  # Use walk animation for right direction
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_v = false
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		$AnimatedSprite2D.animation = "walk"  # Use walk animation for right direction
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_v = false
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		$AnimatedSprite2D.animation = "up"  # Use walk animation for left direction
+		$AnimatedSprite2D.flip_v = true
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		$AnimatedSprite2D.animation = "up"  # Use walk animation for left direction
+		$AnimatedSprite2D.flip_v = false
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		#$AnimatedSprite2D.animation = "walk"
+		#$AnimatedSprite2D.flip_v = false
+		## See the note below about the following boolean assignment.
+		#$AnimatedSprite2D.flip_h = velocity.x < 0
+	#elif velocity.y != 0:
+		#$AnimatedSprite2D.animation = "up"
+		#$AnimatedSprite2D.flip_v = velocity.y > 0
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite2D.play()
+		else:
+			$AnimatedSprite2D.stop()
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
